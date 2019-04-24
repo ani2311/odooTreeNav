0ab101afae2d9a2c1ecf57f67fffecd9bf4c3307
willynpi <peiying.c@tiongliong.com>
2019-04-24 14:57:03 +0800

    edit readme and delete some unrelevent files

--- __manifest__.py
+++ __manifest__.py
@@ -1,30 +1,25 @@
 # -*- coding: utf-8 -*-
 {
     'name': "treeNav",
-
     'summary': """
-        Tree style Menu """,
-
+        Tree style Navigator """,
     'description': """
-        Tree style Menu 
+        Tree style navigator on left side, but still remain the original web style navbar for smaller screen. 
     """,
 
-    'author': "TL",
+    'author': "ynonsi",
     'website': "",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    # 'depends': ['base'],
 
     # always loaded
     'data': [
         'static/src/xml/template.xml',
     ],
+
     # only loaded in demonstration mode
     'demo': [
     #    'demo/demo.xml',
